ea31dba8cc085f4199a6141ced5cb659cfb711a3
bader2337 <brandon.ader@outlook.com>
2015-08-23 18:20:25 -0500

    Created Functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mtrx = NULL
+    set  = function(y) {
+        x <<- y
+        mtrx <<- NULL
+    }
+    get = function() x
+    setInverse = function(solve) mtrx <<- solve
+    getInverse = function() mtrx
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    mtrx = x$getInverse()
+    if(!is.null(mtrx)) {
+        return(m)
+    }
+    data = x$get()
+    mtrx = solve(data, ...)
+    x$setInverse(mtrx)
+    mtrx
+    
 }
